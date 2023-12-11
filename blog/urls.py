52f32a186e90ac8115864d3f4fe79bc44b95a078
Maxime Cardinale <maxime@goodvest.fr>
2023-12-11 10:47:01 +0100

    ajout commentaires

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,28 @@
-from django.urls import path
+from django.urls import path, register_converter
 from . import views
 
 app_name = 'blog'
+
+
+class BooleanConverter:
+    regex = 'True|False'
+
+    def to_python(self, value):
+        return value == 'True'
+
+    def to_url(self, value):
+        return str(value)
+
+
+register_converter(BooleanConverter, "bool")
 
 urlpatterns = [
     # Article CRU(D)
     path('article/create/', views.create_article, name='create_article'),
     path('', views.article_list, name='blog_index'),
     path('article/<uuid:pk>/', views.article_detail, name='article_detail'),
+    path(
+        'article/<uuid:pk>/<bool:incr_views>/', views.article_detail, name='article_detail_from_comment'
+    ),
     path('article/update/<uuid:pk>/', views.update_article, name='update_article'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Article
 from django.core.cache import cache
-from .forms import ArticleForm
+from .forms import ArticleForm, CommentForm
 
 
 @login_required
@@ -48,18 +48,48 @@ def article_list(request):
     return render(request, 'article_list.html', {'articles': articles})
 
 
-def article_detail(request, pk):
+def article_detail(request, pk, incr_views=True):
+    print("INCR_VIEWS : ", incr_views)
+    # Récupération de l'article à afficher
     article = cache.get(f'article_{pk}')
-    views_key = f'article_views_{pk}'
-    views = cache.get(views_key, 0)
-    cache.set(views_key, views + 1, timeout=None)
     if not article:
         article = Article.objects.get(pk=pk)
         cache.set(f'article_{pk}', article, timeout=300)
 
+    # Création de commentaires
+    comment_form = CommentForm()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.article = article
+            new_comment.author = request.user
+            new_comment.save()
+
+            comments = article.comments.all()
+            cache.set(f'comments_for_{pk}', comments, timeout=300)
+            return redirect('blog:article_detail_from_comment', pk=pk, incr_views=False)
+
+    # Récupération des commentaires
+    comments = cache.get(f'comments_for_{pk}')
+    if not comments:
+        comments = article.comments.all()
+        cache.set(f'comments_for_{pk}', comments, timeout=300)
+    for comment in comments:
+        comment.likes = cache.get(f'comment_likes_{comment.pk}', 0)
+        comment.dislikes = cache.get(f'comment_dislikes_{comment.pk}', 0)
+
+    views_key = f'article_views_{pk}'
+    views = cache.get(views_key, 0)
+    if incr_views:
+        cache.set(views_key, views + 1, timeout=None)
+    views = cache.get(views_key)
+
     return_data = {
         'article': article,
-        'views': views
+        'views': views,
+        'comments': comments,
+        'comment_form': comment_form,
     }
-
     return render(request, 'article_detail.html', return_data)
+
